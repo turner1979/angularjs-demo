44b15d0b0802444faffa5187e5272939be720345
Scott Turner <sturner1979@hotmail.com>
2017-03-28 10:17:16 +0100

    Added .get to retrieve real data. Created seperate template file

--- app/components/crimes-list/crimes-list.component.js
+++ app/components/crimes-list/crimes-list.component.js
@@ -2,14 +2,14 @@
 angular
     .module('crimesList')
     .component('crimesList', {
-        template:   '<ul>' +
-                    '   <li ng-repeat="crime in $ctrl.crimes">{{ crime.name }} happened on {{ crime.date }}</li>' +
-                    '</ul>',
-        controller: function CrimesListController() {
-            this.crimes = [
-                { name : 'Theft', date: '01/05/2016' },
-                { name : 'Arson', date: '10/05/2016' },
-                { name : 'Anti Social Behaviour', date: '20/05/2016' }
-            ]
+        templateUrl: 'components/crimes-list/crimes-list.template.html',
+        controller: function CrimesListController($http) {
+
+            var self = this;
+
+            $http.get('data/crimes.json').then(function(response) {
+                self.crimes = response.data;
+            });
+            
         }
     });
